6a5ae0f099b0c1de7ccedab34405438eaf67bf47
Monika Raab <monika.raab@microsoft.wsei.edu.pl>
2019-06-30 00:47:18 +0200

    2DArray

---
+++ 2DArray/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArray
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+           // Console.WriteLine("numer wiersza z maksymalna suma= " + KtóryWiersz(tab));
+
+            int[,] tab = new int[5, 3] {
+                { 1, 1, 1 }, //wiersz 0 
+                { -2, -3, 2 }, // wiersz 1
+                { 2, -5, 0 }, // wiersz 2
+                { -3, 4, 6 }, //wiersz 3
+                { -6, 6, 3 } }; // wiersz 4
+
+            int maxsuma = tab[0, 0];
+
+           
+            int suma = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                suma = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("{0}", tab[i, j]);
+                    suma = suma + tab[i, j];
+
+                    if (suma > maxsuma)
+                    {
+                        maxsuma = suma;
+                    }
+
+
+                }
+
+                Console.Write(" suma =" + "{0}", suma);
+                Console.WriteLine();
+
+            }
+            Console.WriteLine(maxsuma);
+            Console.WriteLine();
+            
+
+            //int numerwiersza = Array.IndexOf(tab, maxsuma);
+
+            
+            Console.ReadKey();
+
+        }
+
+      
+
+       
+
+
+    }
+}
---
+++ Revert7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revert7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line = Console.ReadLine();
+            int n = int.Parse(line);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < i ; j++)
+                {
+                    Console.Write(" ");
+                    
+                }
+                Console.WriteLine("*");
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
