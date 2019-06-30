13d0d8d78ea37bb811b5096c80944aee484ccb3c
Monika Raab <monika.raab@microsoft.wsei.edu.pl>
2019-06-30 10:32:18 +0200

    update

--- 2DArray/Program.cs
+++ 2DArray/Program.cs
@@ -10,7 +10,7 @@ namespace _2DArray
     {
         public static void Main(string[] args)
         {
-
+            int maxwiersz = 0;
            // Console.WriteLine("numer wiersza z maksymalna suma= " + KtóryWiersz(tab));
 
             int[,] tab = new int[5, 3] {
@@ -35,6 +35,7 @@ namespace _2DArray
 
                     if (suma > maxsuma)
                     {
+                        maxwiersz = i; 
                         maxsuma = suma;
                     }
 
@@ -42,10 +43,12 @@ namespace _2DArray
                 }
 
                 Console.Write(" suma =" + "{0}", suma);
+                
                 Console.WriteLine();
 
             }
             Console.WriteLine(maxsuma);
+            Console.WriteLine(maxwiersz);
             Console.WriteLine();
             
 
